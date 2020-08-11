daf8bb948a3e0ff588a4d59c2f46f067092f2325
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-11 16:26:25 -0400

    file components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { TestComponent } from './test/test.component';
-import { Test2Component } from './test2/test2.component';
+import { TestComponent } from './components/test.component';
+import { Test2Component } from './components/test2.component';
 import { FormsModule } from '@angular/forms';
-import { Test3Component } from './test3/test3.component';
-import { PersonaComponent } from './persona/persona.component';
+import { Test3Component } from './components/test3.component';
+import { PersonaComponent } from '.components/persona/persona.component';
 
 
 @NgModule({

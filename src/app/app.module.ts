cf70261c8459a00c482b977587c7279aa99626fc
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-13 15:44:41 -0400

    Admin, User example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,23 +8,32 @@ import { FormsModule } from '@angular/forms';
 import { Test3Component } from './components/test3.component';
 import { PersonModule } from './modules/person/person.module';
 import { ProductoModule } from './modules/producto/producto.module';
+import { UserModule } from './modules/user/user.module';
+import { AdminModule } from './modules/admin/admin.module';
 
 
 
-  
+
+   
 @NgModule({
   declarations: [
     AppComponent,
     TestComponent,
     Test2Component,
     Test3Component,
+  
   ],
   imports: [ 
     BrowserModule,
     FormsModule,
     PersonModule,
-    ProductoModule
+    ProductoModule,
+    UserModule,
+    AdminModule
   ], 
+  exports:[
+
+  ],
   //servicios
   providers: [],
 

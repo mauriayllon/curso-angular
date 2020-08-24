0e62d008c6893fa754cea57ea92811f6896637ca
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-24 15:11:41 -0400

    child in route module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,6 +22,7 @@ import { HomeComponent } from './home/home.component';
 import { Home1Component } from './home1/home1.component';
 import { Home2Component } from './home2/home2.component';
 
+
   
 
 @NgModule({
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './components/user/user.component';
+import { User1Component } from './components/user1/user1.component';
+import { User2Component } from './components/user2/user2.component';
 
 @NgModule({
   imports: [
@@ -9,7 +11,9 @@ import { UserComponent } from './components/user/user.component';
     UserRoutingModule
   ],
   declarations: [
-    UserComponent
+    UserComponent,
+    User1Component,
+    User2Component
   ],
   exports:[
    

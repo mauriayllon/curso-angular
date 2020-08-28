66e96c9d7c133e79449eed011ddfbd18d42ac24d
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-28 16:34:15 -0400

    back up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
-import { AboutComponent } from './about/about.component';
-import { AdminComponent } from './admin/admin.component';
 import { AppRouting } from './app.routing';
-import { HomeComponent } from './home/home.component';
-import { Home1Component } from './home1/home1.component';
-import { Home2Component } from './home2/home2.component';
 import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import {MatSliderModule} from 
 '@angular/material/slider';
 import {MatIconModule} from '@angular/material/icon';
@@ -20,11 +12,7 @@ import {MatIconModule} from '@angular/material/icon';
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    AdminComponent,
-    HomeComponent,
-    Home1Component,
-    Home2Component,
+
   ],
   imports: [
     BrowserModule,
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,19 +1,8 @@
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AdminComponent } from './admin/admin.component';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { Home2Component } from './home2/home2.component';
-import { Home1Component } from './home1/home1.component';
+
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent, children: [
-    {path: 'home1', component: Home1Component},
-    {path: 'home2', component: Home2Component}
-  ]},
-  {path: 'admin', component: AdminComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'user', loadChildren: () => import('./modules/user/user.module').then( m => m.UserModule)}
 ];
 
 

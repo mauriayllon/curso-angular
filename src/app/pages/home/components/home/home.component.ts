c509f1bfe2c5c91429b91664673b9fa818f66dc2
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-28 21:26:39 -0400

    back up 28 august

--- src/app/pages/home/components/home/home.component.ts
+++ src/app/pages/home/components/home/home.component.ts
@@ -7,6 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  productos=[
+    {
+      description:'aaa',
+      imageUrl:'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId:1,
+      price: 12,
+      title:'Salteña'
+    },
+    {
+      description:'aaa',
+      imageUrl:'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId:1,
+      price: 12,
+      title:'Pizza'
+    },
+    {
+      description:'aa',
+      imageUrl:'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId:1,
+      price: 12,
+      title:'Hamburguesa'
+    },
+    {
+      description:'aa',
+      imageUrl:'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId:1,
+      price: 12,
+      title:'Alitas'
+    },
+    {
+      description:'aa',
+      imageUrl:'https://material.angular.io/assets/img/examples/shiba2.jpg',
+      ownerId:1,
+      price: 12,
+      title:'HotDog'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
 import { HomeRoutingModule } from './home.routing.module';
+import { MatCardModule } from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button'
 
 @NgModule({
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    MatCardModule,
+    MatButtonModule
   ],
   declarations: [
     HomeComponent

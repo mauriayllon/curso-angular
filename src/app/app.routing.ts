6c4796b31f83d695a0ec539d5e18a91873a47d18
mauriayllon <mauricio.ad1707@gmail.com>
2020-08-28 23:21:11 -0400

    back up

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -7,9 +7,10 @@ const routes: Routes = [
   
 ];*/
 const routes: Routes = [
-  {path: '', redirectTo: 'login', pathMatch:'full'},
-  {path:'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
 
+  {path: '', redirectTo: 'login', pathMatch:'full'},
+  
+  {path:'login', loadChildren: () => import('./modules/login/login.module').then(m => m.LoginModule)},
   {path:'pages', loadChildren: () => import('./modules/pages/pages.module').then(m => m.PagesModule)}
 ];
 

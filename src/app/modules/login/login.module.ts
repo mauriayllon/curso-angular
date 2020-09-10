6dc5e469b80f423df3440a1538e77c62c9835b87
mauriayllon <mauricio.ad1707@gmail.com>
2020-09-10 13:41:23 -0400

    1 file modified, 2 added, 2 deleted

--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -9,7 +9,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { AuthService } from '../../shared/services/auth.service';
+import { AuthService } from '../../services/auth.service';
 
 @NgModule({
   imports: [

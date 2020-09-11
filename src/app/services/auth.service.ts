a550183f27314702ffdc60601b6b56abd6c9f920
mauriayllon <mauricio.ad1707@gmail.com>
2020-09-10 20:08:27 -0400

    structure

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,19 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-
+import {catchError, map} from 'rxjs/operators';
 import { environment } from '../../environments/environment';
 
 @Injectable()
 export class AuthService {
 
-  url= environment.auth.apiBaseUrl;
+url= environment.auth.apiBaseUrl;
 
-  key=environment.auth.key;
+key=environment.auth.key;
 
-  constructor(private http : HttpClient) { }
-public login(body : any): Observable <any> {
-return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body)
+constructor(private http : HttpClient) { }
+
+  public login(body: any): Observable<any> {
+    return this.http.post(`${this.url}/v1/accounts:signInWithPassword?key=${this.key}`, body).pipe(map((res: any) => {
+        this.authSucess(res.idToken);
+        return res;
+      })
+    );
+  }
+
+private authSucess(token: string): void{
+  localStorage.setItem('auth', token);
 }
-  
 }
